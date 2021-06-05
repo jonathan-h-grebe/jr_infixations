7ec53dba80417c8e71a2c14a52164b84c1f22a31
Jonathan Hill <jonathanhill381@gmail.com>
2021-06-05 14:03:30 +0900

    stable

--- jri/jri_crawling.py
+++ jri/jri_crawling.py
@@ -9,19 +9,30 @@ import django
 django.setup()
 from db_builder.models import Infixation, Checked_Vids, Checked_CommentThreads
 from django.utils import timezone
+from django.utils.timezone import make_aware
 
 # youtubes api's per request response item limits
 SEARCH_MAX_RESULTS = 50
 COMMENT_THREAD_MAX_RESULTS = 100
 COMMENT_MAX_RESULTS = 100
 
+Checked_Vids.objects.all()
+
+COMMENT_COUNT_UNAVAILABLE = -1
+
+
 API_KEY = os.environ['YOUTUBE_API_KEY']
 
 regex = re.compile(r'[jJ][oO][eE] *"[^"]*" *[rR][oO][gG][aA][nN]')
 
+def get_pages_to_check(res_search, num_of_vids_to_check):
+    if res_search['pageInfo']['resultsPerPage'] == 0:
+        return 0
+    return math.ceil(num_of_vids_to_check/res_search['pageInfo']['resultsPerPage'])
+
 def write_to_log_and_print(logfile, msg):
     logfile.write(msg)
-    print(msg)
+    #print(msg)
 
 def menu_exit():
     print("exiting")
@@ -44,59 +55,174 @@ class MenuOpts(Enum):
     WRITE_INFIXATIONS_TO_FILE = 2
     SEARCH_FOR_NEW_INFIXATIONS = 3
 
-def process_comment(comment, v_id, v_title, ct_id, c_id, c_date):
+def text_from_comment(comment):
+    return comment['snippet']['textOriginal']
+def date_from_comment_stuct(comment):
+    return comment['snippet']['publishedAt']
+def topcommentid_from_thread(thread_object):
+    return thread_object["snippet"]["topLevelComment"]['id']
+def topcommentlines_from_thread(thread_object):
+    return thread_object["snippet"]["topLevelComment"]["likeCount"]
+def top_text_from_thread(thread_object):
+    return thread_object["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
+def top_comment_date_from_thread(thread_object):
+    return thread_object["snippet"]["topLevelComment"]['snippet']['publishedAt']
+def likes_from_comment(comment):
+    return comment['snippet']['likeCount']
+def get_comment_count_from_vid(res_vid):
+    return res_vid["items"][0]["statistics"]["commentCount"]
+
+def top_comments_remaining(res_thread):
+    return True if 'nextPageToken' in res_thread else False
+
+def reply_pages_remaining(res_comments):
+    if 'nextPageToken' in res_comments:
+        return True
+    return False
+
+def top_comments_remaining(res_thread):
+    return True if 'nextPageToken' in res_thread else False
+
+def process_comment(comment, v_id, v_title, ct_id, c_id, c_date, like_count):
     res = regex.search(comment)
     if res:
         print(f'Got infixation: {res[0]}')
         Infixation(infixation=res[0], vid_id=v_id, comment_thread_id=ct_id,\
              comment_id=c_id, vid_title=v_title, \
-                 date_added=datetime.datetime(int(c_date[0:4]),\
+                 date_added= make_aware (datetime.datetime(int(c_date[0:4]),\
                                              int(c_date[5:7]),\
-                                            int(c_date[8:10])
-                                            )).save()
+                                            int(c_date[8:10]))
+                                            ), likes=like_count).save()
 
-def check_one_comment_thread():
-    pass
+def print_first_five_vid_titles(res_search):
+    for x in range(0,min(4, len(res_search['items']))):
+        str = res_search['items'][x]['snippet']['title']
+        if video_has_already_been_checked(res_search['items'][x]['id']['videoId']):
+            str = str + " *** ALREADY CHECKED - will be skipped"
+        print(str)
+    print("...\n")
 
-def check_replies_to_thread(thread_item):
-    if 'replies' not in thread_item:
-        pass
+def save_new_checked_video(videoId, videoTitle):
+    Checked_Vids(vid_id=videoId, vid_title=videoTitle).save()
+
+def check_replies_to_thread(thread_item, videoId, videoTitle, logfile, youtube):
+    if 'replies' not in thread_item: pass
+        
     #If all replies included in the current request response
     elif thread_item['snippet']['totalReplyCount'] == len(thread_item['replies']['comments']):
         for comment in thread_item['replies']['comments']:
-            write_to_log_and_print(logfile, 'reply comment: ' + comment['snippet']['textOriginal']+'\n')
-            process_comment(comment['snippet']['textOriginal'],\
-                videoId, videoTitle, thread_item['id'],thread_item["snippet"]["topLevelComment"]['id'],\
-                    comment['snippet']['publishedAt'])
+            process_comment(text_from_comment(comment), videoId, videoTitle, thread_item['id'],\
+                            comment['id'], date_from_comment_stuct(comment), 
+                            likes_from_comment(comment))
+                     
     #If further requests are neccesary to get all the comments.
     else:
         write_to_log_and_print(logfile, f'Getting all comments from "comment" resource.\n')
         res_comments = youtube.do_req(api_type="comments", part="snippet", parentId=thread_item["id"], maxResults=COMMENT_MAX_RESULTS)
-        reply_pages_remaining = True
-        while reply_pages_remaining:
+
+        while True:
             for comment in res_comments['items']:
-                write_to_log_and_print(logfile, f'Comment: {comment["snippet"]["textOriginal"]}\n')
-                process_comment(comment['snippet']['textOriginal'],\
-                    videoId, videoTitle, thread_item['id'],thread_item["snippet"]["topLevelComment"]['id'],\
-                        comment['snippet']['publishedAt'])
-  
-            reply_pages_remaining = True if 'nextPageToken' in res_comments else False
+                write_to_log_and_print(logfile, f'Comment: {text_from_comment(comment)}\n')
 
-            if reply_pages_remaining:
-                res_comments = youtube.do_req(api_type="comments", part="snippet", parentId=thread_item["id"], maxResults=COMMENT_MAX_RESULTS, pageToken=res_comments['nextPageToken'])
+                process_comment(text_from_comment(comment), videoId, videoTitle,\
+                                thread_item['id'], comment['id'], date_from_comment_stuct(comment), 
+                                likes_from_comment(comment))     
 
+            if reply_pages_remaining(res_comments):
+                res_comments = youtube.do_req(api_type="comments", part="snippet",
+                                              parentId=thread_item["id"], 
+                                              maxResults=COMMENT_MAX_RESULTS,
+                                              pageToken=res_comments['nextPageToken'])
+            else: break
+
+def video_has_already_been_checked(videoId):
+    if len(Checked_Vids.objects.filter(vid_id=videoId)) > 0:
+        return True
+    return False
 
 def show_menu_options():
     print('Choose one of...')
     for i in MenuOpts:
         print (str(i.value) + ' ' + i.name)
 
+def search_for_new_infixations():
+    inp = input('enter search string/ or 0 to go back: ')
+
+    with open('get.log', 'a+', encoding='UTF-8') as logfile:
+
+        youtube = youtube_api_wrapper()
+
+        logfile.write(f'STARTING AT {timezone.now()}\n')
+        write_to_log_and_print(logfile, f'Searching for: {inp}\n')
+        res_search = youtube.do_req(api_type="search", part="snippet", q=inp, type="video", maxResults=SEARCH_MAX_RESULTS)
+
+        write_to_log_and_print(logfile, f'\nTotal of {res_search["pageInfo"]["totalResults"]} results\n')
+        print(f'Total of {res_search["pageInfo"]["totalResults"]} results\n')
+        print_first_five_vid_titles(res_search)
+
+        num_of_vids_to_check = int(input('How many of those results do want to check?'))
+        search_pages_to_check = get_pages_to_check(res_search, num_of_vids_to_check)
+        
+        
+        checkRes = input(f'That will require checking {search_pages_to_check} page(s) - and approximately - are you sure?(Y/n) ')
+        if checkRes == 'Y' or checkRes == 'y':
+            
+            while num_of_vids_to_check > 0 and search_pages_to_check > 0:
+
+                for vid in res_search['items']:
+                    #Even if we haven't done every vid in this page of video results, we might have looked through
+                    #the number of videos requested to check.
+                    if num_of_vids_to_check == 0:
+                        break
+
+                    videoId = vid['id']['videoId']
+                    videoTitle = vid['snippet']['title']
+                    if video_has_already_been_checked(videoId):
+                        write_to_log_and_print(logfile, f'Video in search already checked : {videoTitle} - {videoId}\n')
+                    else:
+                        res_vid = youtube.do_req(api_type="videos", part="statistics,snippet", id=videoId)
+                        commentCnt = get_comment_count_from_vid(res_vid)
+                        write_to_log_and_print(logfile, f'Comment count for {videoId} is: {commentCnt}\n')
+
+                        res_thread = youtube.do_req(api_type="commentThreads", part="snippet,replies,id",
+                                                    maxResults=COMMENT_THREAD_MAX_RESULTS, videoId=videoId)
+                        has_more_top_comments = True
+                        while has_more_top_comments:
+
+                            for thread in res_thread['items']:
+                                try:
+                                    write_to_log_and_print(logfile, f'Top level comment: {top_text_from_thread(thread)}\n')
+                                except Exception as e:
+                                    print(f'{e}\n')
+
+                                process_comment(top_text_from_thread(thread), videoId, videoTitle, thread['id'],\
+                                                topcommentid_from_thread(thread), top_comment_date_from_thread(thread),\
+                                                COMMENT_COUNT_UNAVAILABLE)
+
+                                write_to_log_and_print(logfile, f'Has {thread["snippet"]["totalReplyCount"]} reply(ies)\n')
+
+                                check_replies_to_thread(thread, videoId, videoTitle, logfile, youtube)
+                            
+                            has_more_top_comments = top_comments_remaining(res_thread)
+                            if has_more_top_comments:
+                                res_thread = youtube.do_req(api_type="commentThreads", part="snippet,replies,id", maxResults=COMMENT_THREAD_MAX_RESULTS, videoId=videoId, pageToken=res_thread['nextPageToken'])
+
+                    save_new_checked_video(videoId, videoTitle)
+                    num_of_vids_to_check -= 1
+                
+                search_pages_to_check -= 1
+                if search_pages_to_check > 0:
+                    res_search = youtube.do_req(api_type="search", part="snippet", q=inp, type="video", maxResults=SEARCH_MAX_RESULTS, pageToken=res_search['nextPageToken'])
+
+        logfile.write(f'ENDING - used a total of {api_req_cnt} api requests\n\n')
+        logfile.close()
+
+
 if __name__ == "__main__":
 
     api_req_cnt = 0
     
     print('Starting...')
-
 
     while True:
 
@@ -114,100 +240,5 @@ if __name__ == "__main__":
             pass
 
         elif choice == MenuOpts.SEARCH_FOR_NEW_INFIXATIONS.value:
-            inp = input('enter search string/ or 0 to go back: ')
-
-            with open('get.log', 'a+', encoding='UTF-8') as logfile:
-
-                youtube = youtube_api_wrapper()
-
-                logfile.write(f'STARTING AT {timezone.now()}\n')
-                write_to_log_and_print(logfile, f'Searching for: {inp}\n')
-                res_search = youtube.do_req(api_type="search", part="snippet", q=inp, type="video", maxResults=SEARCH_MAX_RESULTS)
-
-                write_to_log_and_print(logfile, f'Total of {res_search["pageInfo"]["totalResults"]} results\n')
-                num_of_vids_to_check = int(input('How many of those results do want to check?'))
-                search_pages_to_check = math.ceil(num_of_vids_to_check/res_search['pageInfo']['resultsPerPage'])
-                
-                checkRes = input(f'That will require checking {search_pages_to_check} page(s) - and approximately - are you sure?(Y/n) ')
-                if checkRes == 'Y' or checkRes == 'y':
-                    
-                    while num_of_vids_to_check > 0 and search_pages_to_check > 0:
-
-                        for vid in res_search['items']:
-                            #Even if we haven't done every vid in this page of video results, we might have looked through
-                            #the number of videos requested to check.
-                            if num_of_vids_to_check == 0:
-                                break
-
-                            videoId = vid['id']['videoId']
-                            videoTitle = vid['snippet']['title']
-                            if len(Checked_Vids.objects.filter(vid_id=videoId)) > 0:
-                                write_to_log_and_print(logfile, f'Video in search already checked : {videoTitle} - {videoId}\n')
-                            else:
-                                res_vid = youtube.do_req(api_type="videos", part="statistics,snippet", id=videoId)
-                                commentCnt = res_vid["items"][0]["statistics"]["commentCount"]
-                                write_to_log_and_print(logfile, f'Comment count for {videoId} is: {commentCnt}\n')
-
-                                res_thread = youtube.do_req(api_type="commentThreads", part="snippet,replies,id", maxResults=COMMENT_THREAD_MAX_RESULTS, videoId=videoId)
-
-                                has_more_top_comments = True
-
-                                while has_more_top_comments:
-
-                                    for thread in res_thread['items']:
-                                        try:
-                                            write_to_log_and_print(logfile, f'Top level comment: {thread["snippet"]["topLevelComment"]["snippet"]["textOriginal"]}\n')
-                                        except Exception as e:
-                                            print(e)
-                                            print("")
-
-                                        process_comment(thread["snippet"]["topLevelComment"]["snippet"]["textOriginal"],\
-                                            videoId, videoTitle, thread['id'],thread["snippet"]["topLevelComment"]['id'],\
-                                                thread["snippet"]["topLevelComment"]['snippet']['publishedAt'])
-
-                                        write_to_log_and_print(logfile, f'Has {thread["snippet"]["totalReplyCount"]} reply(ies)\n')
-
-                                        if 'replies' not in thread:
-                                            write_to_log_and_print(logfile, 'No replies to that comment.\n')
-                                            pass
-                                        elif thread['snippet']['totalReplyCount'] == len(thread['replies']['comments']):
-                                            write_to_log_and_print(logfile, f'All replies fitted in the top level comment request.\n')
-                                            for comment in thread['replies']['comments']:
-                                                write_to_log_and_print(logfile, 'reply comment: ' + comment['snippet']['textOriginal']+'\n')
-                                                process_comment(comment['snippet']['textOriginal'],\
-                                                        videoId, videoTitle, thread['id'],thread["snippet"]["topLevelComment"]['id'],\
-                                                            comment['snippet']['publishedAt'])
-                                        #When we need to get replies page by page from a top-level comment
-                                        else:
-                                            write_to_log_and_print(logfile, f'Getting all comments from "comment" resource.\n')
-                                            res_comments = youtube.do_req(api_type="comments", part="snippet", parentId=thread["id"], maxResults=COMMENT_MAX_RESULTS)
-                                            reply_pages_remaining = True
-                                            while reply_pages_remaining:
-                                                for comment in res_comments['items']:
-                                                    write_to_log_and_print(logfile, f'Comment: {comment["snippet"]["textOriginal"]}\n')
-                                                    process_comment(comment['snippet']['textOriginal'],\
-                                                        videoId, videoTitle, thread['id'],thread["snippet"]["topLevelComment"]['id'],\
-                                                            comment['snippet']['publishedAt'])
-                                                
-                                                reply_pages_remaining = True if 'nextPageToken' in res_comments else False
-
-                                                if reply_pages_remaining:
-                                                    res_comments = youtube.do_req(api_type="comments", part="snippet", parentId=thread["id"], maxResults=COMMENT_MAX_RESULTS, pageToken=res_comments['nextPageToken'])
-                                    
-                                    has_more_top_comments = True if 'nextPageToken' in res_thread else False
-
-                                    if has_more_top_comments:
-                                        res_thread = youtube.do_req(api_type="commentThreads", part="snippet,replies,id", maxResults=COMMENT_THREAD_MAX_RESULTS, videoId=videoId, pageToken=res_thread['nextPageToken'])
-
-                            checked_vid = Checked_Vids(vid_id=videoId, vid_title=videoTitle)
-                            checked_vid.save()
-                            num_of_vids_to_check -= 1
-                        
-                        search_pages_to_check -= 1
-                        if search_pages_to_check > 0:
-                            res_search = youtube.do_req(api_type="search", part="snippet", q=inp, type="video", maxResults=SEARCH_MAX_RESULTS, pageToken=res_search['nextPageToken'])
-
-                logfile.write(f'ENDING - used a total of {api_req_cnt} api requests\n\n')
-                logfile.close()
-
+            search_for_new_infixations()
 
