f346e46fc3e856b3389465d23705d25817b3d637
Jonathan Hill <jonathanhill381@gmail.com>
2021-06-27 15:25:00 +0900

    changed settings

--- jri/jri/settings.py
+++ jri/jri/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
