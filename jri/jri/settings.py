4acf6fc6dc12bdf7d714e1491392ae9da17d117c
Jonathan Hill <jonathanhill381@gmail.com>
2021-06-27 15:21:12 +0900

    changed settings so they rely on env vars

--- jri/jri/settings.py
+++ jri/jri/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -64,6 +64,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jri.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -104,6 +111,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = 'c:/work/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
