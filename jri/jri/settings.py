1036a1dbf826d3f36dd978c40136eb5fcb1863c9
Jonathan Hill <jonathanhill381@gmail.com>
2021-06-27 19:50:04 +0900

    altered settings

--- jri/jri/settings.py
+++ jri/jri/settings.py
@@ -19,7 +19,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',
+                 ' https://sleepy-ridge-39968.herokuapp.com/'
+                 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
