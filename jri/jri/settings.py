da77a938bc371200b69f6ba79026a8d827dffa92
Jonathan Hill <jonathanhill381@gmail.com>
2021-06-28 20:39:52 +0900

    added whitenoise to project

--- jri/jri/settings.py
+++ jri/jri/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = 'c:/work/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
